872d987be8d172b11b7543d722abae6a734ee177
Adriel da Silva <silvaadriel510@gmail.com>
2020-05-09 19:59:22 -0300

    Initial commit

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,34 @@
+import { startOfHour } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  constructor(private appointmentsRepository: AppointmentsRepository) {}
+
+  public execute({ provider, date }: Request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw new Error('This appointment is already booked.');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
