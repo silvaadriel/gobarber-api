80dd257c03bbd2ccac23c0e0be0cf4fc5b19a7e5
Adriel da Silva <silvaadriel510@gmail.com>
2020-06-09 23:03:05 -0300

    Improve code structure

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,13 +1,16 @@
+import 'reflect-metadata';
+
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
-import routes from './routes';
+import routes from '@shared/infra/http/routes';
 
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 
-import './database';
+import '@shared/infra/typeorm';
+import '@shared/container';
 
 const app = express();
 
